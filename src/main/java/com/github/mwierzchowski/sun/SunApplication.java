a0357c5f3f44c62c17028d99099e454788330ff2
Marcin Wierzchowski <mwierzch@gmail.com>
2021-01-23 15:59:36 +0100

    Remove unused frameworks

--- src/main/java/com/github/mwierzchowski/sun/SunApplication.java
+++ src/main/java/com/github/mwierzchowski/sun/SunApplication.java
@@ -4,18 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.time.Clock;
 
 @EnableAsync
 @EnableCaching
 @EnableScheduling
-@EnableJpaAuditing
-@EnableTransactionManagement
 @SpringBootApplication
 public class SunApplication {
 	@Bean
