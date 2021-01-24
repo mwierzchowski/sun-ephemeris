945784cbc6459e455de6db0c5502505338890031
Marcin Wierzchowski <mwierzch@gmail.com>
2021-01-24 17:26:26 +0100

    Add events publisher

--- src/main/java/com/github/mwierzchowski/sun/SunApplication.java
+++ src/main/java/com/github/mwierzchowski/sun/SunApplication.java
@@ -1,14 +1,20 @@
 package com.github.mwierzchowski.sun;
 
+import com.github.mwierzchowski.sun.core.SunEventPublisher;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Clock;
 
+@Slf4j
 @EnableAsync
 @EnableCaching
 @EnableScheduling
@@ -17,6 +23,15 @@ public class SunApplication {
 	@Bean
 	Clock systemClock() {
 		return Clock.systemDefaultZone();
+	}
+
+	@Bean
+	@ConditionalOnProperty(name = "sun-ephemeris.init-on-startup")
+	ApplicationListener<ApplicationReadyEvent> initializer() {
+		return event -> {
+			LOG.info("Initializing on startup");
+			event.getApplicationContext().getBean(SunEventPublisher.class).planEvents();
+		};
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/github/mwierzchowski/sun/core/SunEventPublisher.java
@@ -0,0 +1,73 @@
+package com.github.mwierzchowski.sun.core;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.time.LocalDate;
+
+import static java.time.Duration.ofSeconds;
+import static java.util.UUID.randomUUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SunEventPublisher {
+    public static final String QUEUE_NAME = "sun-ephemeris.events";
+    public static final String LOCK_NAME = "sun-ephemeris.publish-lock";
+
+    private final SunEphemerisProvider provider;
+    private final Clock clock;
+    private final TaskScheduler scheduler;
+    private final RedisTemplate<String, Object> redis;
+    private final Environment env;
+
+    @Retry(name = "SunEventPublisher", fallbackMethod = "planEventsFallback")
+    @Scheduled(cron = "${sun-ephemeris.publish.cron}")
+    public void planEvents() {
+        LOG.info("Planning today events");
+        var today = LocalDate.now(clock);
+        provider.sunEphemerisFor(today).stream()
+                .forEach(this::planEvent);
+    }
+
+    public void planEventsFallback(Throwable throwable) {
+        LOG.warn("Planning events based on today ephemeris failed, trying yesterday", throwable);
+        var yesterday = LocalDate.now(clock).minusDays(1);
+        provider.sunEphemerisFor(yesterday).stream()
+                .map(oldEvent -> new SunEvent(oldEvent.getType(), oldEvent.getTimestamp().plusSeconds(24 * 60 * 60)))
+                .forEach(this::planEvent);
+    }
+
+    private void planEvent(SunEvent event) {
+        if (event.isStale(clock)) {
+            LOG.warn("Event {} will not be published today since it passed at {}",
+                    event.getType(), event.getLocalDateTime(clock).toLocalTime());
+            return;
+        }
+        scheduler.schedule(new Task(event), event.getTimestamp());
+        LOG.info("Event {} scheduled for publishing today at {}", event.getType(), event.getLocalDateTime(clock).toLocalTime());
+    }
+
+    @RequiredArgsConstructor
+    private class Task implements Runnable {
+        private final SunEvent event;
+
+        @Override
+        public void run() {
+            var duration = env.getRequiredProperty("sun-ephemeris.publish.lock-duration", Long.class);
+            if (!redis.opsForValue().setIfAbsent(LOCK_NAME, randomUUID(), ofSeconds(duration))) {
+                LOG.debug("Dropping event {} (not a leader)", event.getType());
+                return;
+            }
+            LOG.info("Publishing event {}", event.getType());
+            redis.convertAndSend(QUEUE_NAME, event);
+        }
+    }
+}
