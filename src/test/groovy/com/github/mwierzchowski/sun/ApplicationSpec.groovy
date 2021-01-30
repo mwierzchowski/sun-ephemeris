5be3e161b7671a07c432fa88afa7bc2e28c96887
Marcin Wierzchowski <mwierzch@gmail.com>
2021-01-30 18:34:53 +0100

    Refactor test

--- src/test/groovy/com/github/mwierzchowski/sun/ApplicationSpec.groovy
+++ src/test/groovy/com/github/mwierzchowski/sun/ApplicationSpec.groovy
@@ -4,18 +4,27 @@ import com.github.mwierzchowski.sun.core.SunEventPublishScheduler
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationListener
 import spock.lang.Specification
 
 @Integration(properties = ["sun-ephemeris.init-on-startup=true"])
-class InitializerSpec extends Specification {
+class ApplicationSpec extends Specification {
+    @Autowired
+    ApplicationContext context
+
     @Autowired
     ApplicationListener<ApplicationReadyEvent> initializer
 
     @SpringBean
     SunEventPublishScheduler publishScheduler = Mock()
 
-    def "Should schedule events"() {
+    def "Should start"() {
+        expect:
+        context != null
+    }
+
+    def "Should initialize"() {
         expect:
         initializer != null
     }
